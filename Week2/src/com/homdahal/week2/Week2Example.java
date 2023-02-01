e4b9a2624a4c2ff6e8f4b73dba7df0845fb7b3c6
homdahal997 <homdahal997@gmail.com>
2023-02-01 12:38:28 -0500

    week2-2

--- Week2/src/com/homdahal/week2/Week2Example.java
+++ Week2/src/com/homdahal/week2/Week2Example.java
@@ -2,11 +2,18 @@ package com.homdahal.week2;
 
 public class Week2Example {
     public static void main(String[] args) {
-        int age = 10;
-        if (age > 19){
+        int age = 64;
+        if (age >= 19){
             System.out.println(" You are an adult");
+            if (age > 65){
+                System.out.println(" You are a senior");
+            }
+        }else if (age >= 13){
+            System.out.println("You are a teenager");
+        } else if (age == 12) {
+            System.out.println(" You are a preteen");
         }else {
-            System.out.println("You are not an adult");
+            System.out.println(" You are a child");
         }
         System.out.println(" Your age is " + age);
     }
