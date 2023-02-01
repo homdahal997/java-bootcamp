5d4aa5b370cc8c1293c8c0efb9193f69864aaae6
homdahal997 <homdahal997@gmail.com>
2023-02-01 12:21:36 -0500

    week2-1-1

--- Week2/src/com/homdahal/week2/Week2Example.java
+++ Week2/src/com/homdahal/week2/Week2Example.java
@@ -3,5 +3,6 @@ package com.homdahal.week2;
 public class Week2Example {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(" Another Hello World ");
     }
 }
