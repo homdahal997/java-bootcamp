4cc8babb954d82388dab81573e3145f3c3dd5916
homdahal997 <homdahal997@gmail.com>
2023-02-01 12:32:14 -0500

    week2-2

--- Week2/src/com/homdahal/week2/Week2Example.java
+++ Week2/src/com/homdahal/week2/Week2Example.java
@@ -2,7 +2,12 @@ package com.homdahal.week2;
 
 public class Week2Example {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println(" Another Hello World ");
+        int age = 10;
+        if (age > 19){
+            System.out.println(" You are an adult");
+        }else {
+            System.out.println("You are not an adult");
+        }
+        System.out.println(" Your age is " + age);
     }
 }
