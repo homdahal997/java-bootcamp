0ee125b2f16059c8688eb7861316c67c51aeb42a
homdahal997 <homdahal997@gmail.com>
2023-02-03 09:01:03 -0500

    Access ArrayList Element

---
+++ ArrayList/src/com/homdahal/ArraylistPractice/ArrayListExample.java
@@ -0,0 +1,33 @@
+package com.homdahal.ArraylistPractice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // create an ArrayList of String type
+        ArrayList<String> names = new ArrayList<>();
+
+        // add John to Arraylist
+        names.add("John");
+
+        // add Harry to Arraylist
+        names.add("Harry");
+
+        // add Niall to Arraylist
+        names.add("Niall");
+
+        // take input from the user
+        Scanner input = new Scanner(System.in);
+        int index = input.nextInt();
+
+        // access the element at index
+        String element = names.get(index);
+
+        // print the element
+        System.out.println(element);
+
+        input.close();
+
+    }
+}
