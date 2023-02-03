105a0881d8917ff47f038518978e8ee3aa560734
homdahal997 <homdahal997@gmail.com>
2023-02-03 10:44:00 -0500

    Modify Elements of an ArrayList

--- ArrayList/src/com/homdahal/ArraylistPractice/ArrayListExample.java
+++ ArrayList/src/com/homdahal/ArraylistPractice/ArrayListExample.java
@@ -9,17 +9,23 @@ public class ArrayListExample {
         ArrayList<String> names = new ArrayList<>();
 
         // add John to Arraylist
-        names.add("Roise");
+        names.add("Jeremy");
 
         // add Harry to Arraylist
-        names.add("Smith");
+        names.add("Jules");
 
         // add Niall to Arraylist
-        names.add("James");
+        names.add("Howard");
+
+        // take String input from the user
+        Scanner input = new Scanner(System.in);
+        String name1 = input.nextLine();
+
+
         // Printing original arrayList
         System.out.println(" Original arrayList - " + names);
         // Changing element at index 1
-        names.set(1, "Steve");
+        names.set(2, name1);
         // Printing updated Arraylist
         System.out.println(" Updated ArrayList - " + names);
 
